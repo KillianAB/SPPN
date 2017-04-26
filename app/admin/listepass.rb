4cac45a29af2556f06832957410632aead017f2f
Axel Avocat-Benan <axel.avocat@gmail.com>
2017-04-26 17:22:10 +0200

    ajout de mail a listepass

---
+++ app/admin/listepass.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Listepass do
+  permit_params :first_name, :last_name, :diplome, :age, :passmorgiou, :passsormiou, :siret, :email
+
+    index do
+      selectable_column
+      column :first_name
+      column :last_name
+      column :email
+      column :age
+      column :diplome
+      column :passmorgiou
+      column :passsormiou
+      column :siret
+      actions
+    end
+
+end
