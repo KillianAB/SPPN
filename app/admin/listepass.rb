2f7f04aad6fbbaab8776ed68e0ee76d104f013af
Axel Avocat-Benan <axel.avocat@gmail.com>
2017-04-26 17:32:50 +0200

    liste pass 2

--- app/admin/listepass.rb
+++ app/admin/listepass.rb
@@ -1,18 +1,12 @@
 ActiveAdmin.register Listepass do
-<<<<<<< HEAD
+
   permit_params :first_name, :last_name, :diplome, :age, :passmorgiou, :passsormiou, :siret, :email
-=======
-  permit_params :first_name, :last_name, :diplome, :age, :passmorgiou, :passsormiou, :siret,
->>>>>>> 4726699b192b9fe94e6f35db086b1da6d7f51eed
 
     index do
       selectable_column
       column :first_name
       column :last_name
-<<<<<<< HEAD
       column :email
-=======
->>>>>>> 4726699b192b9fe94e6f35db086b1da6d7f51eed
       column :age
       column :diplome
       column :passmorgiou
