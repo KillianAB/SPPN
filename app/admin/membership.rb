6a75f08cd3f06399b44c4a4307d3bb333f425b5b
Axel Avocat-Benan <axel.avocat@gmail.com>
2017-04-26 19:19:49 +0200

    mail suppr

--- app/admin/membership.rb
+++ app/admin/membership.rb
@@ -1,12 +1,11 @@
 ActiveAdmin.register Membership do
-  permit_params :first_name, :last_name, :age, :mail, :diplome, :passmorgiou, :passsormiou, :siret, :user_id
+  permit_params :first_name, :last_name, :age, :diplome, :passmorgiou, :passsormiou, :siret, :user_id
 
     index do
       selectable_column
       column :first_name
       column :last_name
       column :age
-      column :mail
       column :diplome
       column :passmorgiou
       column :passsormiou
